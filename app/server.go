0ddfde8f3f033c5dadf9f3a0fdf825aa648044aa
Leonardo Nava <L3oNav@outlook.com>
2024-05-09 11:04:02 -0700

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -6,6 +6,9 @@ import (
 	"net"
 	"os"
 )
+// global variable to store HTTP/1.1 200 OK\r\n\r\n
+var OK_HTTP_1 = []byte("HTTP/1.1 200 OK\r\n\r\n")
+
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -14,12 +17,16 @@ func main() {
 	// Uncomment this block to pass the first stage
 
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
+
+  // send 200 OK
+  client, err = l.Accept()
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
+  client.Write(OK_HTTP_1)
+  defer l.Close() 
 
-	_, err = l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
