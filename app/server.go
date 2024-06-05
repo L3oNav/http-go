49a699345801338acdce1242fe24d2712e9ed001
Leonardo Nava <L3oNav@outlook.com>
2024-06-05 15:20:41 -0700

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -2,37 +2,50 @@ package main
 
 import (
 	"fmt"
-	"net"
+  "net"
+  "bufio"
+	"net/http"
 	"os"
 )
 // global variable to store HTTP/1.1 200 OK\r\n\r\n
 var OK_HTTP_1 = []byte("HTTP/1.1 200 OK\r\n\r\n")
 
+func Handler(conn net.Conn) {
+  defer conn.Close()
+  // read the request
+  request, err := http.ReadRequest(bufio.NewReader(conn))
+  if err != nil {
+    fmt.Println("Error reading request: ", err.Error())
+    return
+  }
+  if request.URL.Path == "/" {
+    conn.Write(OK_HTTP_1)
+    return
+  }
+  conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+}
+
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
-
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+  l, err := net.Listen("tcp", ":4221")
   if err != nil {
-    fmt.Println("Failed to bind to port 4221")
+    fmt.Println("Error listening: ", err.Error())
     os.Exit(1)
   }
 
-  // send 200 OK
-  conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
-  
-		os.Exit(1)
-	}
-  conn.Write(OK_HTTP_1)
-  defer l.Close() 
+  defer l.Close()
 
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+  conn, err := l.Accept()
+  if err != nil {
+    fmt.Println("Error accepting: ", err.Error())
+    os.Exit(1)
+  }
+
+  Handler(conn)
+
 }
+
+
+
